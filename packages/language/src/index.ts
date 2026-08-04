1f9aeabd7f46773723601ab1f26381457c55aa8a
soaibsafi <soaib.safi@gmail.com>
2026-08-04 14:27:08 +0200

    Migrated to langium 4

---
+++ packages/language/src/index.ts
@@ -0,0 +1,5 @@
+export * from './smt-module.js';
+export * from './smt-validator.js';
+export * from './generated/ast.js';
+export * from './generated/grammar.js';
+export * from './generated/module.js';
